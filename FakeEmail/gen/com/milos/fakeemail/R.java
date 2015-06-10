03ad1abe639e11e3675bfdcacdbab4a0094fb4e6
Ascii91 <milos2906@gmail.com>
2015-06-11 00:32:34 +0200

    d

--- FakeEmail/gen/com/milos/fakeemail/R.java
+++ FakeEmail/gen/com/milos/fakeemail/R.java
@@ -8,6 +8,12 @@
 package com.milos.fakeemail;
 
 public final class R {
+    public static final class anim {
+        public static final int activity_close_scale=0x7f040000;
+        public static final int activity_close_translate=0x7f040001;
+        public static final int activity_open_scale=0x7f040002;
+        public static final int activity_open_translate=0x7f040003;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -18,9 +24,9 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int edit_height=0x7f040002;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int edit_height=0x7f050002;
     }
     public static final class drawable {
         public static final int atach=0x7f020000;
@@ -31,27 +37,27 @@ public final class R {
         public static final int send=0x7f020005;
     }
     public static final class id {
-        public static final int Subject=0x7f080002;
-        public static final int action_atach=0x7f080004;
-        public static final int action_send=0x7f080005;
-        public static final int action_settings=0x7f080006;
-        public static final int compose=0x7f080003;
-        public static final int from=0x7f080000;
-        public static final int to=0x7f080001;
+        public static final int Subject=0x7f090002;
+        public static final int action_atach=0x7f090004;
+        public static final int action_send=0x7f090005;
+        public static final int action_settings=0x7f090006;
+        public static final int compose=0x7f090003;
+        public static final int from=0x7f090000;
+        public static final int to=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int compose=0x7f050005;
-        public static final int from=0x7f050002;
-        public static final int subject=0x7f050004;
-        public static final int to=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int compose=0x7f060005;
+        public static final int from=0x7f060002;
+        public static final int subject=0x7f060004;
+        public static final int to=0x7f060003;
     }
     public static final class style {
         /** 
@@ -79,14 +85,14 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int CustomActionBarTheme=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int CustomActionBarTheme=0x7f070002;
         /**  ActionBar styles 
          */
-        public static final int MyActionBar=0x7f060003;
+        public static final int MyActionBar=0x7f070003;
     }
 }
